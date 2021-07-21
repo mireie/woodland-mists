c2fd1351339c69fcee58ea6cea3d0275d74859ae
mireie <michael.reiersgaard@gmail.com>
2021-07-21 13:26:38 -0700

    pass find test
    Co-authored-by: Gengur123 <Gengur123@users.noreply.github.com>
    Co-authored-by: professional-pigeon <professional-pigeon@users.noreply.github.com>
    Co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -20,4 +20,22 @@ describe ("#artist") do
       expect(Artist.all).to(eq([]))
     end
   end
+
+  describe (".") do
+    it("clears the Artist class values") do
+      artist1 = Artist.new(nil, "Manilow and the Berries", "Wizard Funktronic", nil, "6 AM")
+      artist1.save
+      Artist.clear
+      expect(Artist.all).to(eq([]))
+    end
+  end
+  describe ('.find') do
+    it("finds an artist by id") do
+      artist1 = Artist.new(nil, "Manilow and the Berries", "Wizard Funktronic", nil, "6 AM")
+      artist1.save
+      artist2 = Artist.new(nil, "Frank and the Furters", "Sausage Rock", nil, "6:01 AM")
+      artist2.save
+      expect(Artist.find(artist1.id)).to(eq(artist1))
+    end
+  end
 end
