5df53ac025fd29e9daa33a66bd8337971eb76fac
mireie <michael.reiersgaard@gmail.com>
2021-07-21 13:32:38 -0700

    pass delete test
    Co-authored-by: Gengur123 <Gengur123@users.noreply.github.com>
    Co-authored-by: professional-pigeon <professional-pigeon@users.noreply.github.com>
    Co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -29,7 +29,7 @@ describe ("#artist") do
       expect(Artist.all).to(eq([]))
     end
   end
-  describe ('.find') do
+  describe (".find") do
     it("finds an artist by id") do
       artist1 = Artist.new(nil, "Manilow and the Berries", "Wizard Funktronic", nil, "6 AM")
       artist1.save
@@ -38,12 +38,23 @@ describe ("#artist") do
       expect(Artist.find(artist1.id)).to(eq(artist1))
     end
   end
-  describe('#update') do
+  describe("#update") do
     it("updates an artist by id") do
       artist1 = Artist.new(nil, "Manilow and the Berries", "Wizard Funktronic", nil, "6 AM")
       artist1.save()
       artist1.update("Frank and the Furters", "Polish Sausage Rock", nil, "6:01 AM")
       expect(artist1.name).to(eq("Frank and the Furters"))
+    end
+  end
+  describe("#delete") do
+    it("deletes an artist by id") do
+      Artist.clear
+      artist = Artist.new(nil, "Manilow and the Berries", "Wizard Funktronic", nil, "6 AM")
+      artist.save()
+      artist2 = Artist.new(nil, "Frank and the Furters", "Sausage Rock", nil, "6:01 AM")
+      artist2.save()
+      artist.delete()
+      expect(Artist.all).to(eq([artist2]))
     end
   end
 end
