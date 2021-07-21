2cf0e9851ca50412ad52d2b5e5a68a404c3f094f
mireie <michael.reiersgaard@gmail.com>
2021-07-21 11:13:09 -0700

    project setup
    co-authored-by: Kyle Kay-Perez <kyle.kayperez@gmail.com>
    co-authored-by: Arthur Lee <meleearthur@gmail.com>

---
+++ lib/stage.rb
@@ -0,0 +1,41 @@
+class Stage
+  attr_reader :id, :name, :location, :capacity
+  @@stages = {}
+  @@stage_id = 0
+
+  def initialize(id, name, location, capacity)
+    @id = id || @@stage_id += 1
+    @name = name
+    @location = location
+    @capacity = capacity
+  end
+  
+  def self.all
+    @@stages.values()
+  end
+
+  def save
+    @@stages[self.id] = Stage.new(self.id, self.name, self.location, self.capacity)
+  end
+  
+  def self.clear
+    @@stages = {}
+    @@stage_id = 0
+  end
+
+  def ==(stage_to_compare)
+    self.name() == stage_to_compare.name
+  end
+
+  def self.find(id)
+    @@stages[id]
+  end
+
+  def update(id, name, location, capacity)
+  
+  def delete
+    @@stages.delete(self.id)
+  end
+
+
+end
